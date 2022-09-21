3a8e22a25dd0fcdceb4d16e83fa6aafa582f99ec
sajid9962 <clashking1545@gmail.com>
2022-09-22 00:34:35 +0600

    guns component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Guns from './Components/Guns/Guns';
 
 function App() {
   return (
     <div className="App">
-     
+     <Guns/>
     </div>
   );
 }
---
+++ src/Components/Guns/Guns.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Guns = () => {
+	const [guns, setGuns] = useState([]);
+
+	return (
+		<div>
+			<h1>This is from Guns</h1>
+		</div>
+	);
+};
+
+export default Guns;
